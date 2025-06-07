1b0b68eebeb068d472321e01b7767f1031abb9ff
iahub4metra <buhai.artem7@gmail.com>
2025-06-07 14:25:51 +0100

    added notFoundPage

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,6 +3,13 @@ import { Route, Routes } from 'react-router';
 import Header from '../Header/Header';
 
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const RegisterPage = lazy(
+    () => import('../../pages/RegisterPage/RegisterPage'),
+);
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'));
+const NotFoundPage = lazy(
+    () => import('../../pages/NotFoundPage/NotFoundPage'),
+);
 
 export default function Layout() {
     return (
@@ -12,13 +19,12 @@ export default function Layout() {
                 <Suspense fallback={<p>Loading...</p>}>
                     <Routes>
                         <Route path="/" element={<HomePage />} />
-                        <Route path="/auth">
-                            <Route path="register" />
-                            <Route path="login" />
-                        </Route>
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/login" element={<LoginPage />} />
                         <Route path="/news" />
                         <Route path="/notices" />
                         <Route path="/friends" />
+                        <Route path="*" element={<NotFoundPage />} />
                     </Routes>
                 </Suspense>
             </main>
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router';
+
+export default function NotFoundPage() {
+    return (
+        <section className="adaptive-container">
+            <div className=" bg-[#F6B83D] rounded-[30px] flex flex-col items-center py-[245px] px-[33px] md:py-[241px] md:px-[32px] xl:py-[109px] xl:px-[283px]">
+                <div className="flex items-center gap-[8px] md:gap-0 mb-[20px] md:mb-[40px]">
+                    <p className="text-[120px] font-extrabold md:text-[300px] text-white">
+                        4
+                    </p>
+                    <div>
+                        <picture>
+                            <source
+                                srcSet="/public/images/not-found-mobile.png 1x, /public/images/not-found-mobile@2x.png 2x"
+                                media="(max-width: 767px)"
+                            />
+                            <source
+                                srcSet="/public/images/not-found-tablet.png 1x, /public/images/not-found-tablet@2x.png 2x"
+                                media="(min-width: 768px)"
+                            />
+                            <source
+                                srcSet="/public/images/not-found-desc.png 1x, /public/images/not-found-desc@2x.png 2x"
+                                media="(min-width: 1280px)"
+                            />
+                            <img
+                                src="/public/images/not-found-mobile.png"
+                                alt=""
+                                className="rounded-full bg-[#FFFFFF1A]"
+                            />
+                        </picture>
+                    </div>
+                    <p className="text-[120px] font-extrabold md:text-[300px] text-white">
+                        4
+                    </p>
+                </div>
+                <div className="flex flex-col gap-5 items-center">
+                    <p className="mb-[20px] font-bold leading-5 text-[16px] text-white md:text-[24px] md:leading-7">
+                        Ooops! This page not found :(
+                    </p>
+                    <Link
+                        className="text-[#F6B83D] font-bold text-[14px] leading-[18px] bg-[#FFF4DF] py-[12px] px-[30px] md:py-[14px] rounded-[30px] md:text-[16px] md:leading-5"
+                        to="/"
+                    >
+                        To home page
+                    </Link>
+                </div>
+            </div>
+        </section>
+    );
+}
