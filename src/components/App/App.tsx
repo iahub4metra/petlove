6d9902d679f2dd8c23f22d49a2d0474860e52b77
iahub4metra <buhai.artem7@gmail.com>
2025-06-26 19:13:15 +0100

    loading screen

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from 'react';
 import Layout from '../Layout/Layout';
 import MobileMenu from '../MobileMenu/MobileMenu';
+import { useDispatch } from 'react-redux';
+import { getCurrentUser } from '../../redux/auth/operations';
+import type { AppDispatch } from '../../redux/store';
+import LoadingScreen from '../LoadingScreen/LoadingScreen';
+import { useLocation } from 'react-router';
 
 export default function App() {
+    const [isLoading, setIsLoading] = useState(false);
+    const dispatch: AppDispatch = useDispatch();
+    const location = useLocation();
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            dispatch(getCurrentUser(token));
+        }
+    });
+
+    useEffect(() => {
+        setIsLoading(true);
+        const timeout = setTimeout(() => {
+            setIsLoading(false);
+        }, 500);
+        return () => clearTimeout(timeout);
+    }, [location.pathname]);
+
     return (
         <div>
             <Layout />
             <MobileMenu />
+            {isLoading && <LoadingScreen />}
         </div>
     );
 }
---
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -0,0 +1,76 @@
+import { Box, CircularProgress, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { FaHeart } from 'react-icons/fa6';
+
+export default function LoadingScreen() {
+    const [progress, setProgress] = useState(85);
+    //const [isVisible, setIsVisible] = useState(true);
+    const isLoading = progress < 100;
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((prev) => Math.min(prev + 3, 100));
+        }, 70);
+        return () => {
+            clearInterval(timer);
+        };
+    }, [progress]);
+
+    return (
+        <div className="w-[100vw] h-[100vh] bg-[#0000004D] fixed top-0 left-0 loading-image flex justify-center items-center">
+            {isLoading ? (
+                <Box
+                    sx={{
+                        position: 'relative',
+                        display: 'inline-flex',
+                        textAlign: 'center',
+                    }}
+                >
+                    <CircularProgress
+                        size={396}
+                        variant="determinate"
+                        value={progress}
+                        thickness={0.4}
+                        sx={{
+                            '& circle': {
+                                stroke: '#FFFFFF',
+                                opacity: '0.3',
+                            },
+                        }}
+                    />
+                    <Box
+                        sx={{
+                            top: 0,
+                            left: 0,
+                            bottom: 0,
+                            right: 0,
+                            position: 'absolute',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Typography
+                            variant="caption"
+                            component="div"
+                            sx={{
+                                color: '#fff',
+                                fontFamily: 'Manrope',
+                                fontSize: '80px',
+                                letterSpacing: '-3.2px',
+                                lineHeight: '80px',
+                                fontWeight: '700',
+                            }}
+                        >{`${Math.round(progress)}%`}</Typography>
+                    </Box>
+                </Box>
+            ) : (
+                <p className="text-white text-[50px] leading-[50px] tracking-[-2px] font-bold contents md:text-[100px] md:leading-[100px] md:tracking-[-4px]">
+                    petl
+                    <FaHeart className="fill-[#F6B83D] w-[44px] h-[44px] md:w-[82px] md:h-[82px]" />
+                    ove
+                </p>
+            )}
+        </div>
+    );
+}
