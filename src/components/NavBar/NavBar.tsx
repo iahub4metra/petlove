dee920e4c2605d3f4250a8bbc5b9ab7d1ce21fee
iahub4metra <buhai.artem7@gmail.com>
2025-06-28 14:47:03 +0100

    isActive class for NavLinks

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,6 +4,12 @@ interface NavBarProps {
     isMobile?: boolean;
 }
 
+const navLinkClass = ({ isActive }: { isActive: boolean }) => {
+    return `text-[#262626] block text-[16px] py-[15px] px-[20px] rounded-[30px] border-solid border-[1px] border-[#26262626] text-center transition-colors duration-300 active:scale-[0.98] hover:border-[#F6B83D] ${
+        isActive ? 'border-[#F6B83D]' : 'border-[#26262626]'
+    }`;
+};
+
 export default function NavBar({ isMobile = false }: NavBarProps) {
     return (
         <>
@@ -13,26 +19,17 @@ export default function NavBar({ isMobile = false }: NavBarProps) {
                 }`}
             >
                 <li className="max-h-[50px] h-[50px]">
-                    <NavLink
-                        to="/news"
-                        className="text-[#262626] block text-[16px] py-[15px] px-[20px] rounded-[30px] border-solid border-[1px] border-[#26262626] text-center"
-                    >
+                    <NavLink to="/news" className={navLinkClass}>
                         News
                     </NavLink>
                 </li>
                 <li className="max-h-[50px] h-[50px]">
-                    <NavLink
-                        to="/notices"
-                        className="text-[#262626] block text-[16px] py-[15px] px-[20px] rounded-[30px] border-solid border-[1px] border-[#26262626] text-center"
-                    >
+                    <NavLink to="/notices" className={navLinkClass}>
                         Find pet
                     </NavLink>
                 </li>
                 <li className="max-h-[50px] h-[50px]">
-                    <NavLink
-                        to="/friends"
-                        className="text-[#262626] block text-[16px] py-[15px] px-[20px] rounded-[30px] border-solid border-[1px] border-[#26262626] text-center"
-                    >
+                    <NavLink to="/friends" className={navLinkClass}>
                         Our friends
                     </NavLink>
                 </li>
