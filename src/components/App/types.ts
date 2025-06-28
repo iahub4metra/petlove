2db4b8d29b9ade77820fc0cb2a4191782aa073ee
iahub4metra <buhai.artem7@gmail.com>
2025-06-28 14:47:40 +0100

    user types

--- src/components/App/types.ts
+++ src/components/App/types.ts
@@ -1,6 +1,25 @@
-export type User = {
-    _id: string;
+export type BaseUser = {
     name: string;
     email: string;
     token: string;
+};
+
+export type MidUser = BaseUser & {
+    _id: string;
+    noticesFavorites: [
+        {
+            _id: string;
+            species: string;
+            category: string;
+            title: string;
+            name: string;
+            birthday: string;
+            comment: string;
+            sex: string;
+            location: string;
+            imgURL: string;
+            user: string;
+            popularity: number;
+        },
+    ];
 };
