9a37e24c6bedeabb3ffc0e22f438356d7c705815
iahub4metra <buhai.artem7@gmail.com>
2025-06-28 14:48:46 +0100

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,17 +4,19 @@ import { IoMenu } from 'react-icons/io5';
 import NavBar from '../NavBar/NavBar';
 import AuthNav from '../AuthNav/AuthNav';
 import type { AppDispatch } from '../../redux/store';
-import { useDispatch } from 'react-redux';
-import { openMenu } from '../../redux/mobileMenu/slice';
+import { useDispatch, useSelector } from 'react-redux';
+import { openMenu } from '../../redux/uiState/slice';
 import LogOutBtn from '../LogOutBtn/LogOutBtn';
 import UserBar from '../UserBar/UserBar';
+import { selectUser } from '../../redux/auth/selectors';
 
 export default function Header() {
     const dispatch: AppDispatch = useDispatch();
+    const user = useSelector(selectUser);
 
     return (
         <header className="py-8">
-            <div className="flex justify-between items-center adaptive-container">
+            <div className="flex items-center justify-between adaptive-container">
                 <Link
                     to="/"
                     className="flex items-center text-[20px] font-bold text-[#262626] md:text-[28px]"
@@ -25,11 +27,11 @@ export default function Header() {
                 </Link>
                 <NavBar />
                 <div className="flex items-center gap-4">
-                    {<AuthNav />}
-                    {!dispatch && <LogOutBtn />}
-                    {!dispatch && <UserBar />}
+                    {!user && <AuthNav />}
+                    {user && <LogOutBtn />}
+                    {user && <UserBar />}
                     <IoMenu
-                        className="min-xl:hidden w-[32px] h-[32px] md:w-[36px] md:h-[36px] cursor-pointer"
+                        className="min-xl:hidden w-[32px] h-[32px] md:w-[36px] md:h-[36px] cursor-pointer hover:scale-[1.4] hover:rotate-180 duration-500 transition-transform"
                         onClick={() => dispatch(openMenu())}
                     />
                 </div>
