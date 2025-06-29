0fa76ad63cd47980c341fcdef22bc802178aa7b1
iahub4metra <buhai.artem7@gmail.com>
2025-06-29 19:00:47 +0100

    Create OurFriendsPage.tsx

---
+++ src/pages/OurFriendsPage/OurFriendsPage.tsx
@@ -0,0 +1,11 @@
+import PagesTitle from '../../components/PagesTitle/PagesTitle';
+
+export default function OurFriendsPage() {
+    return (
+        <section className="pb-[80px] pt-[26px] md:pt-[52px] xl:pt-[64px]">
+            <div className="adaptive-container">
+                <PagesTitle />
+            </div>
+        </section>
+    );
+}
