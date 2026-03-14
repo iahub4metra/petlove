43b0bbb358f1dd60c7edf929bdcd47e5c0f00609
iahub4metra <buhai.artem7@gmail.com>
2026-03-14 23:59:40 +0000

    style:responsive form title

---
+++ src/components/AddPetForm/AddPetForm.tsx
@@ -0,0 +1,12 @@
+export default function AddPetForm() {
+    return (
+        <div>
+            <h3 className="text-[28px] md:text-[32px] leading-7 md:leading-8 font-bold text-[#262626] tracking-[-0.36px] flex items-center gap-2">
+                Add my pet /{' '}
+                <span className="font-bold text-[14px] md:text-[16px] text-[#2B2B2A40] leading-[18px] md:leading-5">
+                    Personal details
+                </span>
+            </h3>
+        </div>
+    );
+}
