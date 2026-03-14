ed8c278d954840478edf6138218e674e539157a9
iahub4metra <buhai.artem7@gmail.com>
2026-03-14 16:00:12 +0000

    fix: updated PetBlock imports

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,10 @@
 import LoginForm from '../../components/AuthForms/LoginFrom';
-import AuhtPetBlock from '../../components/AuthPetBlock/AuthPetBlock';
+import PetBlock from '../../components/PetBlock/PetBlock';
 
 export default function LoginPage() {
     return (
         <div className="flex flex-col gap-2.5 md:gap-4 xl:gap-8 adaptive-container xl:flex-row">
-            <AuhtPetBlock />
+            <PetBlock />
             <LoginForm />
         </div>
     );
--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,10 +1,10 @@
 import RegistrationForm from '../../components/AuthForms/RegistrationForm';
-import AuhtPetBlock from '../../components/AuthPetBlock/AuthPetBlock';
+import PetBlock from '../../components/PetBlock/PetBlock';
 
 export default function RegisterPage() {
     return (
         <div className="flex flex-col gap-2.5 md:gap-4 xl:gap-8 adaptive-container xl:flex-row">
-            <AuhtPetBlock />
+            <PetBlock />
             <RegistrationForm />
         </div>
     );
