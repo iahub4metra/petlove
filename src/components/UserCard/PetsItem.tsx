0a01edf2133883716358907a79b85e93b0e850a5
iahub4metra <buhai.artem7@gmail.com>
2026-04-01 23:32:28 +0100

    styles: responsive styles

--- src/components/UserCard/PetsItem.tsx
+++ src/components/UserCard/PetsItem.tsx
@@ -29,19 +29,27 @@ export default function PetsItem({ pet }: PetsItemProps) {
     ];
 
     return (
-        <div className="rounded-[30px] border-[#26262615] border-[1px] p-[15px] flex gap-3.5 relative">
+        <div className="rounded-[30px] border-[#26262615] border-[1px] p-[15px] flex gap-3.5 relative md:py-[21px] xl:gap-[25px] xl:items-center xl:p-[19px]">
             <Avatar
                 src={pet.imgURL}
                 sx={{
                     width: '66px',
                     height: '66px',
+                    '@media screen and (min-width: 768px)': {
+                        width: '75px',
+                        height: '75px',
+                    },
+                    '@media screen and (min-width: 1280px)': {
+                        width: '90px',
+                        height: '90px',
+                    },
                 }}
             />
             <div>
-                <h3 className="font-bold text-[14px] leading-[18px] text-[#2B2B2A] overflow-ellipsis overflow-hidden max-w-[145px]">
+                <h3 className="font-bold text-[14px] leading-[18px] text-[#2B2B2A] overflow-ellipsis overflow-hidden max-w-[145px] xl:max-w-[172px]">
                     {pet.title}
                 </h3>
-                <ul className="flex items-center gap-2.5 min-[375px]:gap-3.5 mt-2 flex-wrap">
+                <ul className="flex items-center gap-2.5 mt-2 flex-wrap md:max-w-[173px] xl:max-w-[243px]">
                     {petInfo.map((block, index) => (
                         <li
                             key={index}
@@ -58,7 +66,7 @@ export default function PetsItem({ pet }: PetsItemProps) {
                 </ul>
             </div>
 
-            <div className="rounded-full p-[7px] bg-[#FFF4DF] absolute top-[12px] right-[12px]">
+            <div className="rounded-full p-[7px] bg-[#FFF4DF] absolute top-[12px] right-[12px] xl:p-2.5">
                 <FaRegTrashAlt
                     className="fill-[#F6B83D] w-[18px] h-[18px]"
                     onClick={() => dispatch(removePet(pet._id))}
--- src/components/UserCard/PetsList.tsx
+++ src/components/UserCard/PetsList.tsx
@@ -7,7 +7,7 @@ export default function PetsList() {
     const userPets = user && 'pets' in user ? user.pets : [];
 
     return (
-        <ul className="flex flex-col gap-4 mt-[17px] max-h-[284px] h-[284px] overflow-y-scroll">
+        <ul className="flex flex-col gap-4 mt-[17px] max-h-[284px] h-[284px] overflow-y-scroll md:flex-row md:max-h-[150px] md:h-[150px] md:gap-[13px] md:flex-wrap xl:flex-nowrap xl:flex-col xl:max-h-[274px] xl:h-[274px] custom-scrollbar">
             {userPets.map((pet) => (
                 <li key={pet._id}>
                     <PetsItem pet={pet} />
