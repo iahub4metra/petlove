fe31f123e598410cd7796b4e2dbbbc8516f7bfd3
iahub4metra <buhai.artem7@gmail.com>
2026-04-04 16:57:24 +0100

    feat: created Error component

---
+++ src/components/Errors/ErrorState.tsx
@@ -0,0 +1,22 @@
+export interface ErrorStateProps {
+    error: {
+        message: string;
+        status?: number;
+    } | null;
+}
+
+export default function ErrorState({ error }: ErrorStateProps) {
+    const message =
+        error?.status === 404
+            ? 'Service not found'
+            : error?.status === 500
+              ? 'Server error. Try later'
+              : null;
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-2.5 mt-[40px] md:mt-[80px]">
+            <h2 className="text-5xl">{error?.status}</h2>
+            <p className="text-[16px]">{message}</p>
+        </div>
+    );
+}
