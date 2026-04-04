3a5aed09c48e253c6cccdc1f630d568e6e416c77
iahub4metra <buhai.artem7@gmail.com>
2026-04-04 16:55:14 +0100

    refactor: loading state & error handling

--- src/redux/friends/slice.ts
+++ src/redux/friends/slice.ts
@@ -4,8 +4,6 @@ import { getFriends } from './operations';
 
 interface InitialValue {
     items: Friends[];
-    loading: boolean;
-    error: boolean;
     operations: {
         friends: {
             status: 'idle' | 'loading' | 'succeeded' | 'failed';
@@ -15,14 +13,10 @@ interface InitialValue {
             } | null;
         };
     };
-    status: 'idle' | 'loading' | 'succeeded' | 'failed';
 }
 
 const initialState: InitialValue = {
     items: [],
-    loading: false,
-    error: false,
-    status: 'idle',
     operations: {
         friends: {
             status: 'idle',
@@ -38,24 +32,15 @@ const friendsSlice = createSlice({
     extraReducers: (builder) => {
         builder
             .addCase(getFriends.pending, (state) => {
-                state.loading = true;
-                state.error = false;
-                state.status = 'loading';
                 state.operations.friends.status = 'loading';
                 state.operations.friends.error = null;
             })
             .addCase(getFriends.fulfilled, (state, action) => {
-                state.loading = false;
-                state.error = false;
-                state.status = 'succeeded';
                 state.items = action.payload;
                 state.operations.friends.status = 'succeeded';
                 state.operations.friends.error = null;
             })
             .addCase(getFriends.rejected, (state, action) => {
-                state.error = true;
-                state.loading = false;
-                state.status = 'failed';
                 state.operations.friends.status = 'failed';
                 state.operations.friends.error = action.payload ?? null;
             });
