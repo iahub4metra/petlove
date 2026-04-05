70ea2ba3c4ec31af0420df08dcd0b87f95fdcbd8
iahub4metra <buhai.artem7@gmail.com>
2026-04-06 00:23:02 +0100

    feat: error&loading handling

--- src/components/AuthForms/LoginForm.tsx
+++ src/components/AuthForms/LoginForm.tsx
@@ -7,35 +7,33 @@ import {
     OutlinedInput,
     TextField,
 } from '@mui/material';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
 import AuthTitle from '../AuthTitle/AuthTitle';
-import { useEffect, useRef, useState } from 'react';
 import { MdVisibility, MdVisibilityOff } from 'react-icons/md';
 import FormLink from './FormLink';
-import { registrationSchema } from '../../utils/validationSchema';
+import { useEffect, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { loginSchema } from '../../utils/validationSchema';
+import { yupResolver } from '@hookform/resolvers/yup';
 import CloseIcon from '@mui/icons-material/Close';
 import CheckIcon from '@mui/icons-material/Check';
 import type { AppDispatch } from '../../redux/store';
 import { useDispatch, useSelector } from 'react-redux';
-import { signUp } from '../../redux/auth/operations';
+import { signIn } from '../../redux/auth/operations';
 import { selectAuthOperations } from '../../redux/auth/selectors';
 import ErrorBanner from '../Errors/ErrorBanner';
 import ErrorState from '../Errors/ErrorState';
 
 interface FormValues {
-    name: string;
     email: string;
     password: string;
-    confirmPassword: string;
 }
 
-export default function RegistrationForm() {
+export default function LoginForm() {
     const [showPassword, setShowPassword] = useState(false);
     const [openSnackbar, setOpenSnackbar] = useState(false);
     const dispatch: AppDispatch = useDispatch();
-    const signUpStatus = useSelector(selectAuthOperations).signUp;
-    const prevStatus = useRef(signUpStatus.status);
+    const signInStatus = useSelector(selectAuthOperations).signIn;
+    const prevStatus = useRef(signInStatus.status);
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -53,65 +51,66 @@ export default function RegistrationForm() {
 
     const {
         register,
-        handleSubmit,
         reset,
-        watch,
+        handleSubmit,
         formState: { errors, touchedFields },
     } = useForm<FormValues>({
-        resolver: yupResolver(registrationSchema),
+        resolver: yupResolver(loginSchema),
         mode: 'onBlur',
     });
 
-    const emailError = errors.email;
-    const emailTouched = touchedFields.email;
-
-    const getEmailBorderColor = () => {
-        if (!emailTouched) return '#26262626';
-        if (emailError) return '#EF2447';
-        return '#08AA83';
-    };
-
     const onSubmit = (data: FormValues) => {
-        const userCredentials = {
-            name: data.name,
-            email: data.email,
-            password: data.password,
-        };
-        dispatch(signUp(userCredentials));
-        if (signUpStatus.status === 'succeeded') {
+        const userCredentials = { email: data.email, password: data.password };
+        dispatch(signIn(userCredentials));
+        if (signInStatus.status === 'succeeded') {
             reset();
         }
+    };
+
+    const getEmailBorderColor = () => {
+        if (!touchedFields.email) return '#26262626';
+        if (errors.email) return '#EF2447';
+        return '#08AA83';
     };
 
     useEffect(() => {
         if (
             prevStatus.current !== 'failed' &&
-            signUpStatus.status === 'failed'
+            signInStatus.status === 'failed'
         ) {
             setOpenSnackbar(true);
         }
-        prevStatus.current = signUpStatus.status;
-    }, [signUpStatus.status]);
+        prevStatus.current = signInStatus.status;
+    }, [signInStatus.status]);
 
-    if (signUpStatus.error?.status === 500) {
-        return <ErrorState error={signUpStatus.error} />;
+    if (signInStatus.error?.status === 500) {
+        return <ErrorState error={signInStatus.error} />;
     }
 
     return (
-        <div className="bg-white rounded-[30px] py-[27px] px-[20px] md:py-[30px] md:px-[140px] xl:py-[77px] xl:px-[84px]">
+        <div className="bg-white rounded-[30px] py-[27px] px-[20px] md:py-[30px] md:px-[140px] flex flex-col justify-center xl:px-[84px]">
             <AuthTitle />
             <form
+                autoComplete="false"
                 noValidate
                 onSubmit={handleSubmit(onSubmit)}
-                autoComplete="false"
                 className="flex flex-col gap-[10px] mb-3 md:mb-4"
             >
                 <TextField
-                    {...register('name')}
+                    {...register('email')}
                     variant="outlined"
-                    placeholder="Name"
-                    error={Boolean(errors.name?.message)}
-                    helperText={errors.name?.message}
+                    placeholder="Email"
+                    error={
+                        Boolean(errors.email) ||
+                        Boolean(signInStatus.error?.status === 401)
+                    }
+                    helperText={
+                        errors.email
+                            ? errors.email.message
+                            : signInStatus.error?.status === 401
+                              ? 'Invalid email'
+                              : null
+                    }
                     sx={{
                         '& input:-webkit-autofill': {
                             boxShadow: '0 0 0 1000px white inset',
@@ -121,7 +120,6 @@ export default function RegistrationForm() {
                         },
                         '& .MuiInputBase-root': {
                             borderRadius: '30px',
-                            borderColor: '#26262626',
                             '&:hover .MuiOutlinedInput-notchedOutline': {
                                 borderColor: '#F6B83D',
                             },
@@ -143,60 +141,7 @@ export default function RegistrationForm() {
                         },
                         '& .MuiOutlinedInput-root': {
                             '& fieldset': {
-                                borderColor: '#26262626',
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: '#F6B83D',
-                            },
-                        },
-                    }}
-                />
-                <TextField
-                    {...register('email')}
-                    variant="outlined"
-                    placeholder="Email"
-                    error={
-                        Boolean(errors.email) ||
-                        Boolean(signUpStatus.error?.status === 409)
-                    }
-                    helperText={
-                        errors.email
-                            ? errors.email.message
-                            : signUpStatus.error?.status === 409
-                              ? 'Such email already exists'
-                              : null
-                    }
-                    sx={{
-                        '& input:-webkit-autofill': {
-                            boxShadow: '0 0 0 1000px white inset',
-                            WebkitTextFillColor: '#262626CC',
-                            borderRadius: '30px',
-                            transition: 'background-color 5000s ease-in-out 0s',
-                        },
-                        '& .MuiInputBase-root': {
-                            borderRadius: '30px',
-                        },
-                        '& .MuiOutlinedInput-input': {
-                            padding: '12px',
-                            fontSize: '14px',
-                            color: '#262626CC',
-                            lineHeight: '18px',
-                            '@media screen and (min-width: 768px)': {
-                                padding: '16px',
-                                fontSize: '16px',
-                                lineHeight: '20px',
-                            },
-                        },
-                        '& .MuiOutlinedInput-notchedOutline': {
-                            transition:
-                                'all 250ms cubic-bezier(0.4, 0.2, 0, 0.1)',
-                        },
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
                                 borderColor: getEmailBorderColor(),
-                            },
-                            '&:hover fieldset': {
-                                borderColor: '#F6B83D',
                             },
                             '&.Mui-focused fieldset': {
                                 borderColor: '#F6B83D',
@@ -206,10 +151,10 @@ export default function RegistrationForm() {
                     slots={{ input: OutlinedInput }}
                     slotProps={{
                         input: {
-                            endAdornment: emailTouched && (
+                            endAdornment: touchedFields.email && (
                                 <InputAdornment position="end">
                                     {errors.email ||
-                                    signUpStatus.error?.status === 409 ? (
+                                    signInStatus.error?.status === 401 ? (
                                         <CloseIcon className="text-[#EF2447]" />
                                     ) : (
                                         <CheckIcon className="text-[#08AA83]" />
@@ -229,7 +174,6 @@ export default function RegistrationForm() {
                         },
                         '& .MuiInputBase-root': {
                             borderRadius: '30px',
-                            borderColor: '#26262626',
                             '&:hover .MuiOutlinedInput-notchedOutline': {
                                 borderColor: '#F6B83D',
                             },
@@ -264,7 +208,10 @@ export default function RegistrationForm() {
                         {...register('password')}
                         type={showPassword ? 'text' : 'password'}
                         placeholder="Password"
-                        error={Boolean(errors.password?.message)}
+                        error={
+                            Boolean(errors.password) ||
+                            Boolean(signInStatus.error?.status === 401)
+                        }
                         endAdornment={
                             <InputAdornment position="end">
                                 <IconButton
@@ -292,97 +239,18 @@ export default function RegistrationForm() {
                             </InputAdornment>
                         }
                     />
-                    {errors.password?.message && (
-                        <FormHelperText>
+                    {errors.password?.message ? (
+                        <FormHelperText className="Mui-error">
                             {errors.password.message}
                         </FormHelperText>
-                    )}
-                </FormControl>
-                <FormControl
-                    sx={{
-                        '& input:-webkit-autofill': {
-                            boxShadow: '0 0 0 1000px white inset',
-                            WebkitTextFillColor: '#262626CC',
-                            borderRadius: '30px',
-                            transition: 'background-color 5000s ease-in-out 0s',
-                        },
-                        '& .MuiInputBase-root': {
-                            borderRadius: '30px',
-                            borderColor: '#26262626',
-                            '&:hover .MuiOutlinedInput-notchedOutline': {
-                                borderColor: '#F6B83D',
-                            },
-                        },
-                        '& .MuiOutlinedInput-input': {
-                            padding: '12px',
-                            fontSize: '14px',
-                            color: '#262626CC',
-                            lineHeight: '18px',
-                            '@media screen and (min-width: 768px)': {
-                                padding: '16px',
-                                fontSize: '16px',
-                                lineHeight: '20px',
-                            },
-                        },
-                        '& .MuiOutlinedInput-notchedOutline': {
-                            transition:
-                                'all 250ms cubic-bezier(0.4, 0.2, 0, 0.1)',
-                        },
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
-                                borderColor: '#26262626',
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: '#F6B83D',
-                            },
-                        },
-                    }}
-                    variant="outlined"
-                >
-                    <OutlinedInput
-                        {...register('confirmPassword', {
-                            validate: (value) =>
-                                value === watch('password') ||
-                                'Паролі не співпадають',
-                        })}
-                        type={showPassword ? 'text' : 'password'}
-                        placeholder="Confirm password"
-                        error={Boolean(errors.confirmPassword?.message)}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label={
-                                        showPassword
-                                            ? 'hide the password'
-                                            : 'display the password'
-                                    }
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                    onMouseUp={handleMouseUpPassword}
-                                    edge="end"
-                                    sx={{
-                                        '&:hover': {
-                                            bgcolor: 'transparent',
-                                        },
-                                    }}
-                                >
-                                    {showPassword ? (
-                                        <MdVisibilityOff className="fill-[#F6B83D]" />
-                                    ) : (
-                                        <MdVisibility className="fill-[#F6B83D]" />
-                                    )}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                    {errors.confirmPassword?.message && (
-                        <FormHelperText>
-                            {errors.confirmPassword.message}
+                    ) : signInStatus.error?.status === 401 ? (
+                        <FormHelperText className="Mui-error">
+                            Invalid password
                         </FormHelperText>
-                    )}
+                    ) : null}
                 </FormControl>
                 <Button
-                    loading={signUpStatus.status === 'loading'}
+                    loading={signInStatus.status === 'loading'}
                     type="submit"
                     sx={{
                         marginTop: '22px',
@@ -410,13 +278,13 @@ export default function RegistrationForm() {
                 >
                     Log in
                 </Button>
-                <ErrorBanner
-                    open={openSnackbar}
-                    onClose={() => setOpenSnackbar(false)}
-                    message={signUpStatus.error?.message ?? null}
-                />
             </form>
             <FormLink />
+            <ErrorBanner
+                open={openSnackbar}
+                message={signInStatus.error?.message ?? null}
+                onClose={() => setOpenSnackbar(false)}
+            />
         </div>
     );
 }
